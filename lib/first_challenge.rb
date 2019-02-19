2e3f0615dcb7f3b9db18f90a2dce18c1022fc858
fernando <fernando_lopezc@live.com>
2019-02-19 12:34:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,13 @@ def first_challenge
   }
 
   #your code here
-
+  contacts["Freddy Mercury"].each do 
+  |attribute,data|
+  if data == :favorite_icecream_flavors
+  data.delete_if do |x| x =="strawberry"
+  end
+  end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
